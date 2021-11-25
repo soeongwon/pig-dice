467145ecd40faeda5dc0cbe76bb07b315fe81541
veend <steep_jun_dev@naver.com>
2021-11-25 19:22:49 +0900

    feat: add figdice.js
    plus input code lines

---
+++ pigdice.js
@@ -0,0 +1,16 @@
+// Input Code
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  console.log("Hello Goorm! Your input is", line);
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
+// --------------------
