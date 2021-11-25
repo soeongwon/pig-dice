3802d8b0ab8e02a30ccf84a6de50d04d64a5bb2c
seongwon <ink0507@naver.com>
2021-11-25 21:43:42 +0900

    feat: create pigdice.js

--- pigdice.js
+++ pigdice.js
@@ -1,16 +1,61 @@
-// Input Code
+const players = [
+  { name: '', score: 0},
+  { name: '', score:0}
+];
 
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const curPlayer = 1,
+      gameOver = flase;
 
-rl.on("line", function (line) {
-  console.log("Hello Goorm! Your input is", line);
-  rl.close();
-}).on("close", function () {
-  process.exit();
-});
+players[0].name = prompt('your name, player #1:').toUpperCase();
+players[1].name = prempt('your name, player #2:').toUpperCase();
 
-// --------------------
+function roll() {return 1 + Math.floor(Math.random()*6)}
+
+function round(player) {
+  const curSum = 0,
+        quit = false,
+        dice;
+  alert(`it's ${palyer.name}'s turn (${player.score})`);
+  while (!quit) {
+    dice = roll();
+    if (dice == 1) {
+      alert('you roll a 1.')
+      quit = true;
+    } else {
+      curSum += dice;
+      quit = !confirm(`you roll a ${dice} (sum: ${curSum}).\n roll agin?`);
+      if (quit) {
+        player.score += curSum;
+        if (player.score >= 100) gameOver = true;
+      }
+    }
+  }
+}
+
+//main
+while (!gameOver) {
+  if (curPlayer == 0) curPlayer = 1; else curPlayer = 0;
+  rond(players[curPlayer]);
+  if (gameOver) alert(`${players[curPlayer].name} wins (${players[curPlayer].score})`);
+}
+
+
+
+
+// // Input Code
+
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.on("line", function (line) {
+//   console.log("Hello Goorm! Your input is", line);
+//   rl.close();
+// }).on("close", function () {
+//   process.exit();
+// });
+
+// // --------------------
+
