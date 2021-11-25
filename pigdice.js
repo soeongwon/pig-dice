2769817318c54284ae792ecebc3da3dd4a69ceee
Songhareem <ignisilva92@gmail.com>
2021-11-25 19:45:58 +0900

    feat: add game end check function
    When one player get 100 score, the player win.

--- pigdice.js
+++ pigdice.js
@@ -6,11 +6,28 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.on("line", function (line) {
-  console.log("Hello Goorm! Your input is", line);
-  rl.close();
-}).on("close", function () {
-  process.exit();
-});
+// rl.on("line", function (line) {
+//   console.log("Hello Goorm! Your input is", line);
+//   rl.close();
+// }).on("close", function () {
+//   process.exit();
+// });
+
+function pigDiceGame() {
+  const scores = [100, 0];
+
+  console.log("게임이 시작되었습니다!\n");
+
+  for (;;) {
+    if (scores[0] >= 100) {
+      return "플레이어 1 승리!";
+    }
+    if (scores[1] >= 100) {
+      return "플레이어 2 승리!";
+    }
+  }
+}
+
+console.log(pigDiceGame());
 
 // --------------------
