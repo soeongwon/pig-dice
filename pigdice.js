ba42f7d7e327553cede38cfa77bf68baeeae1776
Songhareem <ignisilva92@gmail.com>
2021-11-25 19:54:14 +0900

    feat: add print game board function

--- pigdice.js
+++ pigdice.js
@@ -18,20 +18,35 @@ function getDiceValue() {
 }
 
 function pigDiceGame() {
-  const scores = [100, 0];
+  let playerNum = 0;
+  const scores = [0, 0];
 
   console.log("게임이 시작되었습니다!\n");
 
   for (;;) {
     if (scores[0] >= 100) {
-      return "플레이어 1 승리!";
+      console.log("플레이어 1 승리!");
+      return;
     }
     if (scores[1] >= 100) {
-      return "플레이어 2 승리!";
+      console.log("플레이어 2 승리!");
+      return;
     }
+
+    console.log("====================================");
+    console.log(`플레이어 ${playerNum + 1} 보드판`);
+    console.log(`지금까지 총점: ${scores[playerNum]}점`);
+    console.log(`1. 주사위 굴리기`);
+    console.log(`2. 넘기기`);
+    rl.on("line", function (line) {
+      console.log("Hello Goorm! Your input is", line);
+      rl.close();
+    });
+
+    break;
   }
 }
 
-console.log(pigDiceGame());
+pigDiceGame();
 
 // --------------------
